076f494a9f535dc6513cbdcce6a8b546e2e97ffd
Erik Lernskog <erik.lernskog@ericsson.com>
2017-12-19 08:27:56 +0100

    Initial commit

---
+++ app/src/main/java/com/lernskog/erik/zombieclient/ZombieClientActivity.java
@@ -0,0 +1,13 @@
+package com.lernskog.erik.zombieclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ZombieClientActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zombie_client);
+    }
+}
